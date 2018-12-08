2b090c9f2b6c22bdd47e294e576f1162abaf6a7b
brtc7d <brtc7d@mail.missouri.edu>
2018-12-07 20:40:44 -0600

    Finished conversion calculator

--- iOSDevConverter/ConverterViewController.swift
+++ iOSDevConverter/ConverterViewController.swift
@@ -16,8 +16,13 @@ struct conversion {
 
 class ConverterViewController: UIViewController {
 
+    
+    
+    //display variables
     @IBOutlet weak var outputDisplay: UITextField!
     @IBOutlet weak var inputDisplay: UITextField!
+    
+    //conversion button popup logic
     @IBAction func converterTapped(_ sender: Any) {
         //UIAlertController with a preferredStyle of UIAlertControllerStyleActionSheet instead
         let actionSheet = UIAlertController(title: "Choose Converter", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
@@ -34,8 +39,111 @@ class ConverterViewController: UIViewController {
     }
     
     var conversions: [conversion] = []
-    var selectedConversionIndex = 0;
+    var selectedConversionIndex = 0
     
+    var inputString = ""
+    
+    
+    //number button functions
+    @IBAction func zeroTapped(_ sender: Any) {
+        if(inputString != "") {
+            addToInput(forPress: 0)
+        }
+    }
+    @IBAction func oneTapped(_ sender: Any) {
+        addToInput(forPress: 1)
+    }
+    @IBAction func twoTapped(_ sender: Any) {
+        addToInput(forPress: 2)
+    }
+    @IBAction func threeTapped(_ sender: Any) {
+        addToInput(forPress: 3)
+    }
+    @IBAction func fourTapped(_ sender: Any) {
+        addToInput(forPress: 4)
+    }
+    @IBAction func fiveTapped(_ sender: Any) {
+        addToInput(forPress: 5)
+    }
+    @IBAction func sixTapped(_ sender: Any) {
+        addToInput(forPress: 6)
+    }
+    @IBAction func sevenTapped(_ sender: Any) {
+        addToInput(forPress: 7)
+    }
+    @IBAction func eightTapped(_ sender: Any) {
+        addToInput(forPress: 8)
+    }
+    @IBAction func nineTapped(_ sender: Any) {
+        addToInput(forPress: 9)
+    }
+    
+    //other buttons
+    @IBAction func clearTapped(_ sender: Any) {
+        inputString = ""
+        updateLabels()
+    }
+    @IBAction func negateTapped(_ sender: Any) {
+        if(inputString != "") {
+            if(inputString.contains("-")) {
+                inputString.remove(at: inputString.startIndex)
+            } else {
+                inputString.insert(Character("-"), at: inputString.startIndex)
+            }
+        }
+        updateLabels()
+    }
+    @IBAction func decimalTapped(_ sender: Any) {
+        if(!inputString.contains(".")) {
+            if(inputString != "") {
+                inputString.append(".")
+            } else {
+                inputString.append("0.")
+            }
+        }
+        updateLabels()
+    }
+    //utility functions
+    func addToInput(forPress number: Int) {
+        inputString.append(String(number))
+        updateLabels()
+    }
+    func formatNumberString(_ str: String) -> String {
+        if(str == "") {
+            return "0"
+        }
+        return str
+    }
+    func getNumberFromString(_ str: String) -> Double {
+        if let lastChar = str.last {
+            if lastChar == "." {
+                inputString.remove(at: inputString.endIndex)
+                let val = Double(inputString)
+                return val!
+            } else {
+                return Double(str)!
+            }
+        }
+        return 0.0
+    }
+    
+    //conversion formulas
+    func applyConversion(to num: Double) -> Double {
+        switch selectedConversionIndex {
+            
+            case 0: // f to c
+                return (num - 32) * 5 / 9
+            case 1: // c to f
+                return num * 9 / 5 + 32
+            case 2: // mi to km
+                return num * 1.609
+            case 3: // km to mi
+                return num / 1.069
+            default:
+                return num
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -49,8 +157,13 @@ class ConverterViewController: UIViewController {
     }
     
     func updateLabels() {
-        outputDisplay.text = conversions[self.selectedConversionIndex].outputUnit
-        inputDisplay.text = conversions[self.selectedConversionIndex].inputUnit
+        
+        inputDisplay.text = formatNumberString(inputString) + conversions[self.selectedConversionIndex].inputUnit
+        
+        let converted = applyConversion(to: getNumberFromString(inputString))
+        
+        
+        outputDisplay.text = formatNumberString(String(converted)) + conversions[self.selectedConversionIndex].outputUnit
     }
 }
 
